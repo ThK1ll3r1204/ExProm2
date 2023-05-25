13655eb0207a4225e9e38c8d586ba4dc6b991c7b
Piero Mazzino <contacto.pieromazzino@gmail.com>
2023-05-25 18:52:06 -0500

    Piero Mazzino cambios en escenas, escritp del main menu y controladorUI

---
+++ Assets/Alumno 2/Boton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boton : MonoBehaviour
+{
+    public Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //button.
+    }
+}
